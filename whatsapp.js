24ffd4a6162b99041d22422a2d6133be27eaeff2
DIEGOCHT <164044510+DIEGOCHT@users.noreply.github.com>
2026-07-06 18:00:47 -0500

    Update whatsapp.js

--- whatsapp.js
+++ whatsapp.js
@@ -3,7 +3,7 @@ window.crearMensajeWhatsApp = function(d){
 return `Hola, deseo consultar el precio del siguiente lote:
 
 Lote: ${d.lote}
-Ubicación: ${d.ubicacion}
+Ubicación: ${d.ubicacion} - ${d.etapa}
 Área: ${d.area}
 
 Nombre: ${d.nombre}
